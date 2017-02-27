bcb3bf6146a2e68f29e40c814c6684da6ede6f06
Leslie Merchan <lesliemerchan71@gmail.com>
2017-02-27 13:21:55 +0000

    smiley face and four corners

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //Write your function calls below this line
+setColor(0, 0, "red");
+setColor(0, 9, "red");
+setColor(9, 0, "red");
+setColor(9, 9, "red");
+
+setColor(3, 3, "blue");
+setColor(3, 6, "blue");
+setColor(5, 2, "blue");
+setColor(6, 3, "blue");
+setColor(6, 4, "blue");
+setColor(6, 5, "blue");
+setColor(6, 6, "blue");
+setColor(5, 7, "blue");
